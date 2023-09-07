7e982d74cd22093b860a99dd9bb58760ceb76414
jajat oray <wiroot92@gmail.com>
2023-09-07 07:51:33 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -11,3 +11,18 @@ module "s3_bucket" {
     enabled = true
   }
 }
+
+module "s3_bucket_for_logs" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "s5-tank-bucket-for-logs"
+  acl    = "log-delivery-write"
+
+  # Allow deletion of non-empty bucket
+  force_destroy = true
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  attach_elb_log_delivery_policy = true
+}
