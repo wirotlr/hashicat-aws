7dae76a01b468828799e4a8be7ad332c63fb1b71
jajat oray <wiroot92@gmail.com>
2023-09-07 08:08:48 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
 
-  prefix_bucket = "s2-bucket"
+  bucket_prefix = "mencret"
   acl    = "private"
 
   versioning = {
