ad705b0860bd424f9718a6d50b6344e89234011a
jajat oray <wiroot92@gmail.com>
2023-09-07 06:38:47 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -197,3 +197,8 @@ resource "aws_key_pair" "hashicat" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.hashicat.public_key_openssh
 }
+
+tags = {
+    Name       = "aws-instance"
+    Department = "devops"  # Add this line for the Department tag
+  }
