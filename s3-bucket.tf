e26816747e9749e74bb906612ebb958ead873250
wirotlr <129021194+wirotlr@users.noreply.github.com>
2023-09-07 14:46:19 +0700

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "s5-tank-bucket"
   acl    = "private"
 
   control_object_ownership = true
